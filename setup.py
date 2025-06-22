27d7fccde98b8d11c1e971c966dbb0e8f37edf38
GregTato180 <gregorio.salomi@edu.unito.it>
2025-06-22 15:39:40 +0200

    Aggiunta librerie

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "geopandas",
+        "seaborn"
+    ],
+)
