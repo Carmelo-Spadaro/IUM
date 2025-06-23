34b77ab7204a2908ce8d155f51c8d1860746aa51
Carmelo-Spadaro <carmelo.spadaro@edu.unito.it>
2025-06-23 11:17:40 +0200

    pulizia dataset actors, crew, languages e genres, modificato setup.py per installazione librerie di sviluppo con `pip install .[notebook]`

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
 
 setup(
+    name="IUM",
+    version="0.1",
     install_requires=[
         "pandas",
         "matplotlib",
         "numpy",
         "scipy",
         "geopandas",
-        "seaborn"
+        "seaborn",
+        "plotly",
     ],
+    extras_require={
+        "notebook": [
+            "notebook",
+            "ipykernel",
+            "jupyterlab",
+        ]
+    }
 )
